0d5e3a7134f70955a05f3bac12cdea74f51f4e1a
daichihasegawa0923 <punyachi@yahoo.co.jp>
2019-11-13 00:52:31 +0900

    プロジェクトファイルを追加

---
+++ ChromeOperator/ChromeOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParseExcelToDataTable;
+
+namespace ChromeOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            driver.Url = "https://google.com";
+            new ParseExcel().GetDataTableFromExcel
+                (@"C:\Users\Daichi Hasegawa\source\repos\ChromeAutomationOperator\Script.xlsx","Sample",true);
+        }
+    }
+}
---
+++ ParseExcelToDataTable/ParseExcel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace ParseExcelToDataTable
+{
+    public class ParseExcel
+    {
+        public DataTable GetDataTableFromExcel(string workBookPath, string sheetName, bool isTopLineIsKind)
+        {
+            var workBook = new XLWorkbook(workBookPath);
+            var sheet = workBook.Worksheet(sheetName);
+            var lastCell = sheet.LastCellUsed();
+            var width = lastCell.Address.ColumnNumber;
+            var height = lastCell.Address.RowNumber;
+            var dataTable = new DataTable();
+            return dataTable;
+        }
+    }
+}
